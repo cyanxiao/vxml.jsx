1fd2b77f1de4397f072322403f44e1972b0de5c0
Chell <im.chell@outlook.com>
2023-04-20 18:03:14 +0200

    feat: block types

---
+++ src/common.d.ts
@@ -0,0 +1,8 @@
+import { JSXNode } from "jsx-xml";
+
+type children = JSXNode;
+type name = string;
+type expr = string;
+type cond = string;
+
+export { children, name, expr, cond };
--- src/components/Block/Block.tsx
+++ src/components/Block/Block.tsx
@@ -1,11 +1,20 @@
 /** @jsx JSXXML */
 
 import { JSXXML } from "jsx-xml";
+import type { JSXNode } from "jsx-xml";
+import type { name, expr, cond } from "../../common";
 
 interface BlockProps {
-  children: string;
+  children?: JSXNode;
+  name?: name;
+  expr?: expr;
+  cond?: cond;
 }
 
-const Block = (props: BlockProps) => <block>{props.children}</block>;
+const Block = (props: BlockProps): JSXNode => (
+  <block name={props.name} expr={props.expr} cond={props.cond}>
+    {props.children}
+  </block>
+);
 
 export default Block;
